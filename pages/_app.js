5bfc4cf64258ddd68cd3f8c9e3b3d85b1ca4fa7c
syasyake <syasyake0401@gmail.com>
2022-07-14 06:24:21 +0900

    remove chakra and install daisyui

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import '../styles/globals.css'
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
-import { Box, Button, Heading, Text } from '@chakra-ui/react'
-import React from 'react'
-
-export default function HomePage() {
-
-return (
-  <Box>
-    <Text>テキスト</Text>
-    <Heading>見出し</Heading>
-    <Button>ボタン</Button>
-  </Box>
+export default function Home() {
+  return (
+    <div>
+      <button className="btn">Button</button>
+    </div>
   )
 }
