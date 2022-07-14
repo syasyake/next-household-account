42fd1fa44a8244de1317c9ae24e85391f1a7a1b1
syasyake <syasyake0401@gmail.com>
2022-07-14 19:08:06 +0900

    add theme

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <div>
-      <button className="btn">Button</button>
+    <div data-theme="cupcake">
+      <div>
+        <button className="btn">Button</button>
+        <select className="select select-primary w-full max-w-xs">
+          <option disabled selected>What is the best TV show?</option>
+          <option>Game of Thrones</option>
+          <option>Lost</option>
+          <option>Breaking Bad</option>
+          <option>Walking Dead</option>
+        </select>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './pages/**/*.{js,ts,jsx,tsx}'
   ],
  theme: {
    extend: {},
- },
- plugins: [require('daisyui')],
+  },
+  darkMode: 'class',
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ["cupcake", "dark"],
+  }
 }
