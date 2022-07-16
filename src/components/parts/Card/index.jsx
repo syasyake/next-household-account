572f9406c9754fb95428e7fc6bd0647687a3994b
syasyake <syasyake0401@gmail.com>
2022-07-16 22:56:06 +0900

    change directory name

---
+++ src/components/parts/Card/index.jsx
@@ -0,0 +1,10 @@
+export default function Card() {
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">Card title!</h2>
+        <p>Card Content</p>
+      </div>
+    </div>
+  )
+}
